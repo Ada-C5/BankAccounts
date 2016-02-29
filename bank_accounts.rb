8e0eead0999981c4a79c067b790978eb38d663bf
Valerie <val.conklin@gmail.com>
2016-02-29 14:49:57 -0800

    Added withdraw method and show_balance method.

--- bank_accounts.rb
+++ bank_accounts.rb
@@ -4,6 +4,9 @@
 # Should have a deposit method that accepts a single parameter which represents the amount of money that will be deposited. This method should return the updated account balance.
 # Should be able to access the current balance of an account at any time.
 
+# use this later when you're ready to deal with converting 1000 into $10.00, etc.
+# require "money"
+
 module Bank
   class Account
     def initialize(id, initial_balance)
@@ -11,5 +14,13 @@ module Bank
       @balance = initial_balance
     end
 
+    def withdraw(amount_to_withdraw)
+      @balance = @balance - amount_to_withdraw
+      show_balance
+    end
+
+    def show_balance
+      puts "The balance for this account is currently #{@balance}."
+    end
   end
 end
