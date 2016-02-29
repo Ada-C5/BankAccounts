d83eff2a22e10c10bffb35abf716108f683139d6
Jade Vance <jadenicolevance@gmail.com>
2016-02-29 15:31:38 -0800

    Added error message for check_new_account.

--- bank-accounts.rb
+++ bank-accounts.rb
@@ -1,15 +1,24 @@
 module Bank
 	
 	class Account
-		attr_accessor :account_id, :balance
+		attr_reader :account_id, :initial_balance
 
-		def initialize(account_id, balance)
+		def initialize(account_id, initial_balance)
 			@account_id = account_id
-			@balance = balance.to_f
+			@balance = initial_balance
+			check_new_account(initial_balance)
+		end
+
+		# check to see if initial balance is less than 0
+		def check_new_account(initial_balance)
+			if initial_balance < 0 
+				raise ArgumentError.new("Starting account balance may not be negative.")
+			end
 		end
 
 		def withdraw_money(amount)
 			new_balance = @balance - amount
+			# Prevents the user from withdrawing more money than they have
 			while new_balance <= 0 
 				puts "*** ERROR ***"
 				puts "Insufficent funds."
@@ -23,6 +32,7 @@ module Bank
 		end
 
 		def deposit_money(amount)
+			# Should there be a way to prevent a deposit of negative money?
 			new_balance = @balance + amount
 			puts format("Depositing $%.2f to your account", @balance)
 			@balance = new_balance
@@ -36,11 +46,8 @@ module Bank
 	end
 end
 
-new_account = Bank::Account.new("12345", "150.00")
-p new_account
-p new_account.show_balance
-p new_account.withdraw_money(45.00)
-p new_account.deposit_money(45.00)
-
-# account ID generation, required externally. 
-# give it an external balance  
+new_account = Bank::Account.new("12345", 150.00)
+puts new_account
+puts new_account.show_balance
+puts new_account.withdraw_money(45.00)
+puts new_account.deposit_money(45.00)
