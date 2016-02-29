95db2ef640a0f2f9be96ac60b28990d240d3a7e1
Lisa Rolczynski <lisa.sano@gmail.com>
2016-02-29 14:55:09 -0800

    Added withdraw, deposit methods.
    If trying to withdraw more money in the account, prints an insufficient funds message and does not take any money out of the account.  Tested both methods with an instance of the Account class.

--- bank-account.rb
+++ bank-account.rb
@@ -11,10 +11,35 @@ module Bank
       @balance = account_info[:initial_balance]
     end
 
+    def withdraw(money)
+      if money <= @balance
+        @balance -= money
+      else
+        puts "Insufficient funds. Withdrawal canceled."
+        @balance
+      end
+    end
+
+    def deposit(money)
+      @balance += money
+    end
+
   end
 end
 
 
 lisa = Bank::Account.new(id: 123456789, initial_balance: 30.00)
 puts "Account id is: #{lisa.id}"
+puts "Account balance is: #{lisa.balance}"
+
+puts "Withdraw 10 dollars."
+lisa.withdraw(10)
+puts "Account balance is: #{lisa.balance}"
+
+puts "Withdraw 500 dollars."
+lisa.withdraw(500)
+puts "Account balance is: #{lisa.balance}"
+
+puts "Deposit 1000 dollars."
+lisa.deposit(1000)
 puts "Account balance is: #{lisa.balance}"
