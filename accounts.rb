7e356a284245c0f1471878fcec5ccf693fd1bc11
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-02-29 14:38:08 -0800

    framework complete including bank module, account and owner classes - no real usability yet

---
+++ accounts.rb
@@ -0,0 +1,38 @@
+require 'Money'
+module Bank
+ class Account
+   attr_accessor :balance
+   attr_reader :id
+
+   def initialize(initial_balance, id_number)
+     @balance = initial_balance
+     @id = id_number
+   end
+
+   def withdraw(amount)
+     if amount > @balance
+       puts "Warning: requested amount is greater than available balance. Please make another selection."
+       return @balance
+     else
+       @balance = @balance - amount
+     end
+   end
+
+   def deposit(amount)
+     @balance = @balance + amount
+   end
+ end
+
+
+ class Owner
+   attr_reader :id
+
+   def initialize(owner_hash)
+     @name = owner_hash[:name]
+     @id = owner_hash[:id]
+     @address = owner_hash[:address]
+   end
+
+
+ end
+end
