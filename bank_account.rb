c1a0e7900b23b99404ac0ea68ad79743c3f7a3eb
Adriana <dri19tcc@gmail.com>
2016-03-01 10:22:41 -0800

    Added type of account to account ex checking, savings.  added more identifiers for owner account.

--- bank_account.rb
+++ bank_account.rb
@@ -3,7 +3,7 @@ module Bank
     attr_reader :account_id, :account_balance, :owner_id
     @@Accounts = []
 
-    def initialize(initial_balance, owner_id)
+    def initialize(initial_balance, type_of_account, owner_id = 0)#owner_id = 0 by default
       if initial_balance < 0
         raise ArgumentError, "Initial balance must be greater than $0.00."
       end
@@ -15,8 +15,13 @@ module Bank
       end
       @@Accounts << @account_id
       @account_balance = initial_balance.to_f.round(2)
+      @type_of_account = type_of_account
       #need to add an owner to the account during initialize
       @owner_id = owner_id
+    end
+
+    def add_owner(id) #account.add_owner(number that you get from Bank::Owner.new)
+      @owner_id = id  #adriana_account.add_owner(adriana_owner.user_id)
     end
 
     def balance
@@ -40,11 +45,13 @@ module Bank
   end
 
   class Owner
-    attr_reader :first_name, :last_name, :user_id
+    attr_reader :first_name, :last_name, :user_id, :city, :state
 
-    def initialize(first_name, last_name)
+    def initialize(first_name, last_name, city, state)
       @first_name = first_name
       @last_name = last_name
+      @city = city
+      @state = state
       @user_id = @first_name[0] + @last_name + rand(111...999).to_s
 
     end
@@ -53,7 +60,7 @@ module Bank
 
 end
 
-#adriana_account = Bank::Account.new("adriana", "cannon", "0")
-#adriana_owner = Bank::Owner.new("adriana", "cannon")
+#adriana_account = Bank::Account.new(500, "checking")
+#adriana_owner = Bank::Owner.new("adriana", "cannon", "el paso", "texas")
 # adriana_account.deposit(200)
 # #adriana_account.withdraw(100)
