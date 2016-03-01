11c7e6ca90b8769be706f338bacf94a20f0a5a2d
Sarah <sarahkane682@yahoo.com>
2016-02-29 16:41:01 -0800

    added owner class that interacts with the account class

--- BankAccounts.rb
+++ BankAccounts.rb
@@ -2,9 +2,10 @@ module Bank
   class Account
     attr_reader :initial_balance
     def initialize(id,initial_balance)
+      @owner = []
       @id = id
       @initial_balance = initial_balance.to_f
-      if @initial_balance < 1.0
+      if @initial_balance < 0.0
         raise ArgumentError.new("Account can not start with a negative balance")
       end
     end
@@ -28,6 +29,26 @@ module Bank
       @initial_balance
     end
 
+    def owner(owner)
+      @owner << owner.owner_hash
+      return @owner
+    end
+
+    def check_owner
+      @owner
+    end
 
   end
+
+  class Owner
+    attr_reader :id, :name, :address, :owner_hash
+    def initialize(id,name,address)
+      @id = id
+      @name = name
+      @address = address.to_s
+      @owner_hash = {id: @id, name: @name, address: @address}
+    end
+  end
+
+
 end
