329291af2105fd9313b9dfab71f2bc685c0be63d
Adriana <dri19tcc@gmail.com>
2016-02-29 21:18:03 -0800

    updated initial balance to account balance.  generated ID in initializer.  put in an Argument error if initial balance < 0.  updated withdraw and deposit methods.

--- bank_account.rb
+++ bank_account.rb
@@ -1,43 +1,50 @@
 module Bank
   class Account
-    attr_reader :first_name, :last_name, :id, :initial_balance
+    attr_reader :first_name, :last_name, :id, :account_balance
 
     def initialize(first_name, last_name, initial_balance)
+      if initial_balance < 0
+        raise ArgumentError, "Initial balance must be greater than $0.00."
+      end
       @first_name = first_name
       @last_name = last_name
-      @id = []
-      @initial_balance = initial_balance.to_f.round(2)
-      @account_balance = []
+      @id = @first_name[0] + @last_name + rand(111...999).to_s
+      @account_balance = initial_balance.to_f.round(2)
+
     end
 
     def balance
-      balance = @initial_balance
-      return balance
-    end
-
-    def identification #this method is creating an identification number and storing it in the empty array
-      @first_name.split
-        fi = @first_name[0]
-      @id << fi + @last_name + rand(111...999).to_s
-      puts @id #test to make sure correct numbers went into empty error
+      return @account_balance
     end
 
     def withdraw(money)
-      balance = @initial_balance - money
-      @account_balance << balance.to_f
-      puts @account_balance
+      balance = @account_balance - money
+      if balance < 0
+        puts "You do not have enough money."
+        return @account_balance #puts "You don't have enough money!"
+      end
+      @account_balance = balance
+      return @account_balance
     end
 
     def deposit(money)
-      balance = @initial_balance + money
-      @account_balance << balance.to_f
-      puts @account_balance
+      @account_balance = @account_balance + money
+      return @account_balance
+    end
+  end
+
+  class Owner
+    attr_reader :first_name, :last_name
+
+    def initialize
+      @first_name =
+      @last_name
     end
 
   end
+
 end
 
-# adriana_account = Bank::Account.new("adriana", "cannon", "0")
-# adriana_account.identification
+#adriana_account = Bank::Account.new("adriana", "cannon", "0")
 # adriana_account.deposit(200)
 # #adriana_account.withdraw(100)
