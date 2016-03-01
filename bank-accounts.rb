f9f6b0f4b5acfe1adacba9a8020bd8814efcf8a1
Jessica <jessica.weeber@gmail.com>
2016-02-29 19:06:01 -0800

    updating account class with owner variable

--- bank-accounts.rb
+++ bank-accounts.rb
@@ -3,12 +3,12 @@ module Bank
   class Account
 
     attr_reader :balance
-    attr_accessor :owners
+    attr_accessor :owner
 
     def initialize(id, balance)
       @id = id
       @balance = balance
-      @owners = {}
+      @owner = owner
       if @balance < 0
           raise ArgumentError, "Balance can't be less than 0"
       end
@@ -29,31 +29,22 @@ module Bank
       puts "New account balance: $#{'%.02f'% @balance}"
     end
 
-    def new_owner(owner)
-      @owners[owner.name] = owner
+    def add_owner(owner)
+      @owner = owner
     end
 
   end
 
   class Owner
 
-    attr_accessor :id, :first_name, :last_name, :street_address
+    attr_accessor :id, :name, :street_address
 
     def initialize(owner_hash)
       @id = owner_hash[:id]
-      @first_name = owner_hash[:first_name]
-      @last_name = owner_hash[:last_name]
+      @name = owner_hash[:name]
       @street_address = owner_hash[:street_address]
     end
 
   end
 
 end
-
-# @jessica = Bank::Owner.new(id: 252525, first_name: "Jessica", last_name: "Weeber", street_address: "123 Main St, Seattle WA 98111",)
-#
-# @new = Bank::Account.new(2525, 100.00)
-#
-# @owners = @new.new_owner(@jessica)
-#
-# puts @owners.first_name
