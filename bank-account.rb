5d100ac7d5407bf364e7a982d9cbed529eafa650
Lisa Rolczynski <lisa.sano@gmail.com>
2016-03-01 16:19:48 -0800

    Added self.all class method in Bank::Accounts.
    This method instantiates Account objects from a CSV file.  Added creation date to Accounts.

--- bank-account.rb
+++ bank-account.rb
@@ -1,16 +1,19 @@
 # Lisa Rolczynski
 # 2016-02-29
 
+require 'csv'
 require 'money'
 I18n.enforce_available_locales = false # need this not to trip an I18n error!
 
+
 module Bank
   class Account
-    attr_reader :id, :balance, :owner
+    attr_reader :id, :balance, :owner, :creation_date
     
     def initialize(account_info)
       @id = account_info[:id]
       @owner = account_info[:owner]
+      @creation_date = account_info[:creation_date]
       @balance = account_info[:initial_balance]
       if @balance < Money.new(0) # if not a money object, comparison with balance won't work
         raise ArgumentError.new("You can't open an account with no money!")
@@ -34,6 +37,26 @@ module Bank
       @owner = owner_obj
     end
 
+    # return a collection of Account instances, representing all of the
+    # Accounts described in the CSV.
+    def self.all
+      accounts = []
+      info_hash = {}
+      CSV.open("support/accounts.csv", 'r').each do |line|
+        info_hash[:id] = line[0]
+        info_hash[:initial_balance] = Money.new(line[1])
+        info_hash[:creation_date] = line[2]
+        accounts << self.new(info_hash)
+      end
+      accounts
+    end
+
+    # return an instance of Account, where the value of the id field in
+    # the CSV matches the passed parameter.
+    def self.find(id)
+
+    end
+
   end
 
   class Owner
@@ -52,6 +75,16 @@ module Bank
 
   end
 
+# return a collection of Owner instances, representing all owners described
+# in the CSV.
+  def self.all()
+  end
+
+# return an instance of Owner where the value of the id field in the CSV
+# matches the passed parameter.
+  def self.find(id)
+  end
+
 end
 
 lisa_owner = Bank::Owner.new(first_name: "Lisa", last_name: "Rolczynski", address_one: 1234, city: "Seattle", state: "WA", zip_code: 98117)
@@ -59,10 +92,12 @@ puts "Owner: #{lisa_owner.first_name} #{lisa_owner.last_name}"
 
 # use the Money gem. By default, the currency is in USD. (to override: Money.new(3000, "EUR"))
 # must change all instances of money to a money object; can't do math with fixnums & money objects
-lisas_account = Bank::Account.new(id: 123456789, initial_balance: Money.new(3000))
+lisas_account = Bank::Account.new(id: 1212, initial_balance: Money.new(1235667), creation_date: "2010-12-21 12:21:12 -0800")
 puts "Account id is: #{lisas_account.id}"
+
 # format numbers to be expressed in dollars (not cents) and include $ symbol using .format
 puts "Account balance is: #{lisas_account.balance.format}"
+puts "Creation date: #{lisas_account.creation_date}"
 
 puts "Withdraw $10."
 lisas_account.withdraw(Money.new(1000))
@@ -77,10 +112,9 @@ lisas_account.deposit(Money.new(100000))
 puts "Account balance is: #{lisas_account.balance.format}"
 
 lisas_account.add_owner(lisa_owner)
-puts "Account owner name is: #{lisas_account.owner.first_name}"
+puts "Account owner lives in: #{lisas_account.owner.city}, #{lisas_account.owner.state}"
 
 
-
-
+accounts = Bank::Account.all
 
 
