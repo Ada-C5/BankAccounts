d94757aaaf06a0c5c0423edc4eb0ee2ed8df1832
Mindy Carson <mcarson1111@gmail.com>
2016-03-01 16:09:47 -0800

    I have absolutely no idea what I'm doing and I don't feel good....hates it

--- BankAccount.rb
+++ BankAccount.rb
@@ -1,14 +1,46 @@
+require 'csv'
+
 module Bank
   class Account
-    attr_accessor :id, :initial_balance
+    attr_accessor :id, :initial_balance, :accounts
 
-    def initialize (id, initial_balance)
+    def initialize (id, initial_balance, accounts)
       @id = id
       @balance = initial_balance
         if @balance < 0
           raise ArgumentError.new("Nope.")   #now, how do i not let initial_balance be a negative? Start over?
         end
+
+          #makes an instance variable accounts with a hash
+        @accounts = {
+
+          "id" => get_account("id"),
+          "amount" => get_account("amount"),
+          "date_opened" => get_account("date_opened")
+
+        }
+
     end
+
+#this method will access the id, amount and date
+#and on each iteration put the cooresponding indexes together in a new array
+#since each line in the csv file is a seperate array, i want to return the ______
+
+#How on earth can we initiate a new instance of an account using this info???
+    def get_accounts()
+      account_array = []
+      CSV.open("accounts.csv", 'r').each do |array|
+        #if mode == array[0]
+          #array.each do |word|
+            #new_array << word
+          end
+        end
+      end
+      #return account_array
+    end
+
+
+
 
     #puts "Welcome to whatever bank, your balance is #{@initial_balance}."
     #puts "What would you like to do; withdraw or deposit?"
