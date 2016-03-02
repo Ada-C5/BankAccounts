493a8b5f750cec3650df08b8c7f017566b366cdf
Cristal Tay <bluecolorpencils@gmail.com>
2016-03-02 09:09:59 -0800

    Reworked the info hash. Figured out a loop to retrieve info from accounts.csv. Creates a new array and inserts a new hash for each row.

--- bank_accounts.rb
+++ bank_accounts.rb
@@ -1,14 +1,13 @@
  # accounts = CSV.read("/Users/cristal/C5/projects/bank-account/BankAccounts/support/accounts.csv")
 require 'csv'
-accounts = CSV.read('accounts.csv')
+# require 'BigDecimal'
+
 module Bank
   class Account
     def initialize(id=0, money=0)
       @id = id
       @money = money
       @initial_money = money
-
-
       if @money < 0
         raise ArgumentError.new("Only positive numbers are allowed in the bank.")
       end
@@ -32,38 +31,64 @@ module Bank
     end
 
     def balance
-     puts "Current balance: $#{@money}"
+      puts @money
+     var = '%0.7s' % @money.to_s
+    #  sprintf("%0.02f", total/100)
+     puts "Current balance: $#{var}"
     end
-
   end
 
-class Owner
-  attr_accessor :name, :address,
+  class Owner
+    # attr_accessor :name, :address,
+    def initialize
+      # @owner_hash = hash
 
-  def initialize(owner_hash)
-    @owner_hash = owner_hash
+      @info_hash = {:list_num => "", :last_name => "",:first_name => "",
+        :street =>"", :city => "", :state => "", :id => "", :initial_balance => ""}
+
+      @accounts = CSV.read('accounts.csv')
+      @account_owners = CSV.read('account_owners.csv')
+      @owners = CSV.read('owners.csv')
+    end
+
+    def info
+      num = 0
+      hashes = []
+      CSV.foreach("owners.csv") do |row|
+        # puts row[1] #gets the second column
+         hashes[num]= @info_hash
+        (0..(@owners[0].length)-1).each do |col|
+          puts row[col]
+          puts "MEOW"
+          hashes[num][@info_hash.keys[col]] = @owners[num][col]
+        end
+        num += 1
+      end
+
+      puts @info_hash
+    end
+
   end
 end
 
 # refer to csv files for info
-owner_hash ={
-  :list_num =>
-  :name => {
-    :first_name =>
-    :last_name =>
-  }
-  :address =>{
-    :street =>
-    :city =>
-    :state =>
-  }
-  :id =>
-  :initial_balance =>
-
-  :open_info => {
-    :date =>
-    :time =>
-  }
-}
-
-end
+# owner_hash ={
+#   :list_num =>
+#   :name => {
+#     :last_name =>
+#     :first_name =>
+#
+#   }
+#   :address =>{
+#     :street =>
+#     :city =>
+#     :state =>
+#   }
+#   :id =>
+#   :initial_balance =>
+#
+#   :open_info => {
+#     :date =>
+#     :time =>
+#   }
+# }
