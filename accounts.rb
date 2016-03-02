57fe69b0d2a78e48f1ec7bb4ef3c0f8d5bd938ce
Ania Gonzalez <aniagr@gmail.com>
2016-03-02 09:23:07 -0800

    Added self.all and self.find methods
    Will have to think about refactoring into separate classes

--- accounts.rb
+++ accounts.rb
@@ -6,14 +6,40 @@ module Bank
   #A new account should be created with an ID and an initial balance
   class Account
     #Should be able to access the current balance of an account at any time.
-    attr_accessor :balance
+    attr_reader :balance, :all_accounts
 
-    def initialize(id, initial_balance)
+    def initialize(id, balance, open_date) #intialize owner to nil
       #A new account cannot be created with initial negative balance - this will raise an ArgumentError
-      raise ArgumentError, "A new account cannot be created with initial negative balance." if initial_balance < 0
+      raise ArgumentError, "A new account cannot be created with initial negative balance." if balance < 0
       @id = id
-      @balance = initial_balance
+      @balance = balance
+      @open_date = open_date
+      #@account_owner = owner
     end
+
+    def self.all(file_path)
+      require 'CSV'
+      @all_accounts = []
+      file_path = "./support/accounts.csv"
+      CSV.foreach(file_path, "r") do |line|
+        @all_accounts << self.new(line[0].to_i, line[1].to_f, line[2])
+      end
+      return @all_accounts
+    end
+
+    def self.find(id)
+      require 'CSV'
+      selected_account = nil
+      file_path = "./support/accounts.csv"
+      CSV.foreach(file_path, "r") do |line|
+        if line[0] == id.to_s
+          selected_account = self.new(line[0].to_i, line[1].to_f, line[2])
+        end
+      end
+      return selected_account
+    end
+
+
 
     # withdraw method that accepts a single parameter which represents the amount of money
     # that will be withdrawn. This method should return the updated account balance.
@@ -36,14 +62,17 @@ module Bank
       return @balance
     end
 
+    def add_owner(owner_inst)
+      @account_owner = owner_inst
+    end
   end
 
   class Owner
 
-    def initialize(owner_hash)
-      @owner_id = owner_hash[:id]
-      @owner_name = owner_hash[:name]
-      @owner_address = owner_hash[:address]
+    def initialize(owner_id, owner_name, owner_address = nil)
+      @owner_id = owner_id
+      @owner_name = owner_name
+      @owner_address = owner_address
     end
 
 
