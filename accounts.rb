db047cc70e88b514230378c9c14091a24b653b46
Anna Wilson <annie.marie.mason@gmail.com>
2016-03-01 22:23:00 -0800

    Began Wave 2 requirements.
    added shells for self.all and self.find(id) methods in Accounts.
    
    successfully linked csv file.
    
    need to figure out how to auto-create new instances of Account using csv data.

--- accounts.rb
+++ accounts.rb
@@ -1,24 +1,39 @@
-module Bank
+#create instances from csv
+#class method to crete all accts
+# newproducts << Product.new(data[0], data[1], data[2], data[3])
 
-  class Owner
-    attr_accessor :name #add address, phone
-    def initialize(owner_hash)
-      @name = owner_hash[:name]
-      #add address, phone after this can get connected with Account class
-    end
-  end
+  # all_accts.length.times do |n|
+  # all_accts[n][0]
+
+# all_accts = CSV.read("./support/accounts.csv")
+# all_accts.length.times do |n|
+#  Bank::Account.new(all_accts[n][0], all_accts[n][1], all_accts[n][2])
+# end
+
+###
+
+module Bank
 
   class Account
     attr_accessor :balance, :id, :amount, :owner, :name
 
-    def initialize(balance, id)
-      unless balance.is_a?(Integer) && balance >= 0
-        raise ArgumentError.new("New accounts must begin with a balance of 0 or more.")
-      end
+    def initialize(id, balance, opendate)
+      # unless balance.is_a?(Integer) && balance >= 0
+      #   raise ArgumentError.new("New accounts must begin with a balance of 0 or more.")
+      # end
       @balance = balance
       @id = id
-      @owner = ""
-      @name = ""
+      @opendate = opendate
+      # @owner = ""
+      # @name = ""
+    end
+
+    def self.all
+#returns a collection of Account instances, representing all of the Accounts described in the CSV.
+    end
+
+    def self.find(id)
+# returns an instance of Account where the value of the id field in the CSV matches the passed parameter
     end
 
     def withdraw(amount)
@@ -41,12 +56,36 @@ module Bank
       puts "Your current balance is $#{@balance}."
     end
 
-    def define_user(name)
-      @name = name #this works now
-      @owner = Bank::Owner.new(name: @name) #still doens't work.
-      puts @owner
-    end
+    # def define_user(name)
+    #   @name = name #this works now
+    #   @owner = Bank::Owner.new(name: @name) #still doens't work.
+    #   puts @owner
+    # end
 
   end
 
+  # class Owner
+  #   attr_accessor :name #add address, phone
+  #   def initialize(owner_hash)
+  #     @name = owner_hash[:name]
+  #     #add address, phone after this can get connected with Account class
+  #   end
+  # end
+
 end
+
+require 'csv'
+
+all_accts = CSV.read("./support/accounts.csv")
+id = all_accts[0][0]
+bal = all_accts[0][1]
+date = all_accts[0][2]
+puts id
+puts bal
+puts date
+
+new_test = Bank::Account.new(100, 200, 300)
+puts new_test
+# all_accts.each do (0..12)
+# puts all_accts[note][0]
+# end
