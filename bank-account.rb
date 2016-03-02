95317f01c079ab4d756ed0770aa4fb1dc573e1ca
Lisa Rolczynski <lisa.sano@gmail.com>
2016-03-02 14:23:48 -0800

    Added self.find(id) Account class method, changed Money object usage, took out "puts" tests at the end.
    Had many places where I converted integers to Money objects, but took those out and created a single method (to_money) that turns balance into a Money object.
    self.find class method runs self.all (to create an array of accounts), iterates through and tries to match id to each instance.  if found, returns the account instance.

--- bank-account.rb
+++ bank-account.rb
@@ -15,9 +15,10 @@ module Bank
       @owner = account_info[:owner]
       @creation_date = account_info[:creation_date]
       @balance = account_info[:initial_balance]
-      if @balance < Money.new(0) # if not a money object, comparison with balance won't work
+      if @balance < 0
         raise ArgumentError.new("You can't open an account with no money!")
       end
+
     end
 
     def withdraw(money)
@@ -27,6 +28,7 @@ module Bank
         puts "Insufficient funds. Withdrawal canceled."
         @balance # return balance without altering it if withdrawal amount is higher than balance
       end
+
     end
 
     def deposit(money)
@@ -37,27 +39,51 @@ module Bank
       @owner = owner_obj
     end
 
+    # turn balance into a Money object.  Format: $X.XX
+    def to_money
+      Money.new(balance).format
+    end
+
     # return a collection of Account instances, representing all of the
     # Accounts described in the CSV.
     def self.all
       accounts = []
       info_hash = {}
-      CSV.open("support/accounts.csv", 'r').each do |line|
+
+      # open CSV file and iterate over each element (which is an array)
+      # each array element contains three components: id, balance, creation date
+      CSV.open("./support/accounts.csv", 'r').each do |line|
+        # index 0 will always be id
+        # index 1 will always be balance (convert to a Money obj)
+        # index 2 will always be creation date (in DateTime)
         info_hash[:id] = line[0]
-        info_hash[:initial_balance] = Money.new(line[1])
+        info_hash[:initial_balance] = line[1].to_i
         info_hash[:creation_date] = line[2]
+        # instantiate using the new hash, then push into accounts array
         accounts << self.new(info_hash)
       end
+
+      # return accounts array which now contains all the new Account objects
       accounts
     end
 
     # return an instance of Account, where the value of the id field in
     # the CSV matches the passed parameter.
     def self.find(id)
+      #loop through until I find the one I'm looking for
+      found_account = nil
+      self.all.each do |line|
+        if id == line.id
+          found_account = line
+          return found_account
+        end
+
+      end
 
     end
 
   end
+
 
   class Owner
     attr_reader :first_name, :last_name, :address_one, :address_two, :city, :state, :zip_code
@@ -78,43 +104,16 @@ module Bank
 # return a collection of Owner instances, representing all owners described
 # in the CSV.
   def self.all()
+
   end
 
 # return an instance of Owner where the value of the id field in the CSV
 # matches the passed parameter.
   def self.find(id)
+
   end
 
 end
 
-lisa_owner = Bank::Owner.new(first_name: "Lisa", last_name: "Rolczynski", address_one: 1234, city: "Seattle", state: "WA", zip_code: 98117)
-puts "Owner: #{lisa_owner.first_name} #{lisa_owner.last_name}"
-
-# use the Money gem. By default, the currency is in USD. (to override: Money.new(3000, "EUR"))
-# must change all instances of money to a money object; can't do math with fixnums & money objects
-lisas_account = Bank::Account.new(id: 1212, initial_balance: Money.new(1235667), creation_date: "2010-12-21 12:21:12 -0800")
-puts "Account id is: #{lisas_account.id}"
-
-# format numbers to be expressed in dollars (not cents) and include $ symbol using .format
-puts "Account balance is: #{lisas_account.balance.format}"
-puts "Creation date: #{lisas_account.creation_date}"
-
-puts "Withdraw $10."
-lisas_account.withdraw(Money.new(1000))
-puts "Account balance is: #{lisas_account.balance.format}"
-
-puts "Withdraw $500."
-lisas_account.withdraw(Money.new(50000))
-puts "Account balance is: #{lisas_account.balance.format}"
-
-puts "Deposit $1000."
-lisas_account.deposit(Money.new(100000))
-puts "Account balance is: #{lisas_account.balance.format}"
-
-lisas_account.add_owner(lisa_owner)
-puts "Account owner lives in: #{lisas_account.owner.city}, #{lisas_account.owner.state}"
-
-
-accounts = Bank::Account.all
 
 
