3d232acab1132b15b6155f868e158d4e6012ddb3
Jessica <jessica.weeber@gmail.com>
2016-03-02 13:53:02 -0800

    class method to find instance when passed id works now. class method to return all instances is working but might not be what we're supposed to return.

--- bank-accounts.rb
+++ bank-accounts.rb
@@ -5,18 +5,19 @@ module Bank
 
   class Account
 
-    attr_reader :balance, :id, :open_date
+    attr_reader   :balance, :id, :open_date
     attr_accessor :owner
 
     def initialize(id, balance, open_date)
-      @id = id.to_i
-      @balance = balance.to_i
+      @id        = id.to_i
+      @balance   = balance.to_i
       @open_date = DateTime.strptime(open_date, '%Y-%m-%d %H:%M:%S %z')
-      @owner = owner
+      @owner     = owner
+
       if @balance < 0
           raise ArgumentError, "Balance can't be less than $0"
       end
-      puts "A new account has been created!"
+
     end
 
     def self.import_account
@@ -24,32 +25,38 @@ module Bank
       CSV.foreach("support/accounts.csv") do |row|
         accounts << Bank::Account.new(row[0], row[1], row[2])
       end
-      pp accounts
+      return accounts
     end
 
-    #returns list of all accounts in CSV file and all info assoc w/them
+    #returns list of all instances of accounts
     def self.all
-
+      accounts = self.import_account
+      return accounts
     end
 
     #returns info on account when passed the id number
     def self.find(id)
-
+      accounts = self.import_account
+      accounts.each do |account|
+        if account.id == id
+          return account
+        end
+      end
     end
 
     def withdraw(withdraw_amount)
       if @balance - withdraw_amount < 0
         puts "You can't withdraw more than is in the account. Choose another amount to withdraw"
-        puts "Current account balance: $#{'%.02f'% @balance}"
+        puts "Current account balance: $#{@balance}"
       else
         @balance -= withdraw_amount
-        puts "New account balance: $#{'%.02f'% @balance}"
+        puts "New account balance: $#{@balance}"
       end
     end
 
     def deposit(deposit_amount)
       @balance += deposit_amount
-      puts "New account balance: $#{'%.02f'% @balance}"
+      puts "New account balance: $#{@balance}"
     end
 
     def add_owner(owner)
@@ -63,7 +70,7 @@ module Bank
     attr_accessor :id, :name, :street_address
 
     def initialize(owner_hash)
-      @id = owner_hash[:id]
+      @id   = owner_hash[:id]
       @name = owner_hash[:name]
       @street_address = owner_hash[:street_address]
     end
@@ -71,7 +78,3 @@ module Bank
   end
 
 end
-
-# CSV.foreach("support/accounts.csv") do |row|
-#   account = Bank::Account.new(row[0], row[1], row[2])
-# end
