98e82db6c47aa398053f8c550a840c56b7b0fda8
Jessica <jessica.weeber@gmail.com>
2016-03-01 18:05:54 -0800

    sorta figured out self methods that will display all accounts in their arraysand another that allows for searching by id number and displays all info for that acct.

--- bank-accounts.rb
+++ bank-accounts.rb
@@ -1,3 +1,14 @@
+require 'CSV'
+require 'pp'
+
+# CSV.open("support/accounts.csv", "w") do |csv|
+#   CSV.foreach('support/accounts.csv') do |row|
+#     csv << row[0].to_i
+#     csv << row[1].to_i
+#     csv << row[2].to_datetime
+#   end
+# end
+
 module Bank
 
   class Account
@@ -5,14 +16,52 @@ module Bank
     attr_reader :balance
     attr_accessor :owner
 
-    def initialize(id, balance)
+    # CSV.open('support/accounts.csv', 'w') do |csv|
+    #   CSV.foreach('original.csv') do |row|
+    #     csv << row[0].to_i
+    #     csv << row[1].to_i
+    #     csv << row[2].to_datetime
+    #   end
+    # end
+
+    # @id = []
+    # @balance = []
+    # @open_date = []
+    # CSV.foreach("support/accounts.csv", converters: :numeric, :date_time) do |row|
+    #   @id << row[0]
+    #   @balance << row[1]
+    #   @open_date << row[2]
+    # end
+
+
+    # need to initialize CSV file and convert data types
+
+    def initialize(id, balance, open_date)
+      # CSV.read("support/accounts.csv" {converters: :numeric, :date_time} )
       @id = id
       @balance = balance
+      @open_date = open_date
       @owner = owner
       if @balance < 0
           raise ArgumentError, "Balance can't be less than $0"
       end
       puts "An account has been created!"
+    end
+
+    #returns list of all accounts in CSV file and all info assoc w/them
+    def self.all
+      CSV.foreach("support/accounts.csv") do |row|
+        pp row
+      end
+    end
+
+    #returns info on account when passed the id number
+    def self.find(id)
+      CSV.foreach("support/accounts.csv") do |row|
+        if row[0] == id
+          pp row
+        end
+      end
     end
 
     def withdraw(withdraw_amount)
@@ -49,3 +98,20 @@ module Bank
   end
 
 end
+
+accts = CSV.read("support/accounts.csv", {converters: :numeric} )
+
+puts accts
+
+# id = []
+# balance = []
+# open_date = []
+# CSV.foreach("support/accounts.csv") do |row|
+#   id << row[0]
+#   balance << row[1]
+#   open_date << row[2]
+# end
+#
+# puts id
+# puts balance
+# puts open_date
