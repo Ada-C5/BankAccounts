cfcf11959cb2d720dba440019a66caf341620c90
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-03-02 14:15:35 -0800

    Primary requirements complete

--- accounts.rb
+++ accounts.rb
@@ -1,16 +1,33 @@
 # require 'Money'
+require 'csv'
+#require_relative './support/accounts'
 module Bank
  class Account
-   attr_accessor :balance
+   attr_accessor :balance, :owner, :accounts_hash
    attr_reader :id
 
-   def initialize(initial_balance, id_number)
-     unless initial_balance > 0
+   def initialize(id_number, initial_balance, account_date)
+     unless initial_balance.to_f > 0
        raise ArgumentError.new("Account must have money in it.")
      end
-     @balance = initial_balance
      @id = id_number
+     @balance = initial_balance
+     @account_date = account_date
    end
+
+  def self.pull_from_csv
+  CSV.read('./support/accounts.csv')
+  end
+
+  def self.all
+    accounts_hash = {}
+    self.pull_from_csv.each do |account|
+      new_account = Bank::Account.new(account[0], account[1].to_f , account[2])
+      id = account[0]
+      accounts_hash[id] = new_account
+    end
+      accounts_hash
+  end
 
    def withdraw(amount)
      if amount > @balance
@@ -24,7 +41,39 @@ module Bank
    def deposit(amount)
      @balance = @balance + amount
    end
- end
+
+   def self.find_owner_number(id) #search accounts by id number
+     owner_list = CSV.read('./support/account_owners.csv')
+     owner_list.each do |account|
+      if account[0] = id
+        return account[1]
+      end
+    end
+   end
+
+  def self.find_owner_info_with_number(id) #USER COMMAND TO FIND OWNER
+    owner_number = self.find_owner_number(id)
+    owner_list = CSV.read('./support/account_owners.csv')
+    puts owner_list
+    puts "read CSV"
+    puts owner_number
+    owner_list.each do |owner|
+      if owner[1] == owner_number
+        return owner_number
+      end
+  end
+
+
+
+
+   def self.find(id)
+    self.all.each do |account|
+      if account[0] = id
+        return account
+      end
+    end
+   end
+  end
 
 
  class Owner
@@ -35,6 +84,10 @@ module Bank
      @id = owner_hash[:id]
      @address = owner_hash[:address]
    end
+
+
+ end
+
 
 
  end
