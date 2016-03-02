6bed3c0176604237e4c3283112093efdcf29777e
Suzanne Harrison <harrison_family@hotmail.com>
2016-03-02 08:54:18 -0800

    added csv info via hash

--- bank_accounts.rb
+++ bank_accounts.rb
@@ -1,16 +1,35 @@
-module Bank
+  require 'CSV'
+  require 'awesome_print'
+
+  module Bank
   class Account
+
     attr_reader :current_balance
-    attr_accessor :id, :initial_balance, :owner
+    attr_accessor :id, :initial_balance, :owner, :accounts
+
 
     def initialize(account)
-      @id = account[:id]
-      @initial_balance = account[:initial_balance]
-      @current_balance = account[:initial_balance]  #when initiating
-      @owner = account[:owner]
+      ##unless account = nil, populate hash info
+      @id = account[:id] #|| @id = nil
+      @initial_balance = account[:initial_balance] #|| @id = nil
+      @current_balance = account[:current_balance]  #|| @current_balance = nil
+      @owner = account[:owner] #|| @owner = nil
+      @start_date = account[:start_date] #|| @start_date = nil
       raise ArgumentError, "We cannot deposit negative amounts. Please enter your deposit amount." unless @current_balance >= 0
     end
 
+    def self.load_file(filename)
+      accounts =[]
+      CSV.open(filename, 'r') do |csv|
+        csv.read.each do |line|
+        accounts << self.new(id: line[0], initial_balance: line[1].to_f, start_date: line[2])
+        end
+      end
+      ap accounts
+    end
+
+    def self.all
+    end
     def withdraw(money)
       if @current_balance < money
         puts "We cannot deposit negative amounts. Please enter your deposit amount."
@@ -53,5 +72,9 @@ module Bank
 end
 
 #@sue = Bank::Owner.new(name: "Suzanne Harrison", street_address_2: "4726 Thackeray Pl NE", city: "Seattle", zip_code: "98105")
-#@owner = Bank::Account.new()
+#@sue = Bank::Account.new()
 #my_account.pass_owner_info(owner) #to pass owner info into Account
+
+#to test the self and csv method
+#my_accounts = Bank::Account.load("./support/accounts.csv")
+#puts accounts[0]
