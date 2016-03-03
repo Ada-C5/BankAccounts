551a47445c8c6afb8db2c330021c9556c589e0a4
justine <justine.winnie@gmail.com>
2016-03-03 13:51:15 -0800

    Added withdraw_with_check method to Checking Account class.

--- bank_accounts.rb
+++ bank_accounts.rb
@@ -219,8 +219,12 @@ module Bank
     # #withdraw_using_check(amount): The input amount gets taken out of the account as a result of a check withdrawal. Returns the updated account balance.
     # Allows the account to go into overdraft up to -$10 but not any lower
     # The user is allowed three free check uses in one month, but any subsequent use adds a $2 transaction fee
-    def withdraw_using_check
-
+    def withdraw_using_check(withdrawal)
+      if @balance - withdrawal >= -10
+        @balance -= withdrawal + withdrawal_fee
+      else puts "Checking account must maintain minimum balance of $-10. Withdrawal cannot be completed with available funds."
+        balance
+      end
     end
 
     #reset_checks: Resets the number of checks used to zero
