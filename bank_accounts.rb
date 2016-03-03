999ea82cb40d666f97ce5349ff775bcdf53707a0
Cristal Tay <bluecolorpencils@gmail.com>
2016-03-03 14:39:53 -0800

    Figured out how to put the loop back in for the self.owner class that reduces the work of having to manually type out each key => each row col in the excel sheet. Added a self.new_account for funsies, if you don't feel like initializing it normally. Reformatted the Account class. Added a SavingsAccount and Checking Account class which follow the traits <Account with changes to certain methods based on withdrawal fees or withdrawal check fees. Wave 3 is complete.

--- bank_accounts.rb
+++ bank_accounts.rb
@@ -1,71 +1,75 @@
- # accounts = CSV.read("/Users/cristal/C5/projects/bank-account/BankAccounts/support/accounts.csv")
-require 'csv'
-# require 'BigDecimal'
+require 'CSV'
+require 'money'
 
 module Bank
   class Account
-    attr_reader :id
-    def initialize(stupid_hash)
-      @id = stupid_hash[:id]
-      @money = stupid_hash[:money]
-      @start_date = stupid_hash[:start_date]
+    attr_reader :id, :money, :start_date
 
+    def initialize(hash)
+      @id = hash[:id]
+      @money = hash[:money]
+      @start_date = hash[:start_date]
 
-      if @money < 0
-        raise ArgumentError.new("Only positive numbers are allowed in the bank.")
+      if money < 0
+        raise ArgumentError.new("Only positive amounts are allowed in the bank.")
       end
     end
 
+    def self.new_account(id = nil, money = nil, start_date =nil)
+      new_hash = self.new(:id => id, :money => money)
+      if money < 0
+        raise ArgumentError.new("Only positive amounts are allowed in the bank.")
+      end
+      return new_hash
+    end
+
     def withdraw(take_out = 0)
-      @take_out = take_out
-      if @money.to_f - @take_out.to_f < 0#@take_out.to_f < 0
+      # @take_out = take_out
+      if @money.to_f - take_out.to_f < 0#@take_out.to_f < 0
         puts "You don't have enough money in the bank for this transaction."
         balance
       else
-        @money -= @take_out.to_f
+        @money -= take_out.to_f
         balance
       end
     end
 
     def deposit(put_in = 0)
-      @put_in = put_in
-      @money += @put_in.to_f
+      # put_in = put_in
+      @money += put_in.to_f
       balance
     end
 
     def balance
-      puts @money
-     var = '%0.7s' % @money.to_s
-    #  sprintf("%0.02f", total/100)
-     puts "Current balance: $#{var}"
+      var = '%0.7s' % @money.to_s
+      #sprintf("%0.02f", total/100)
+      return "Current balance: $#{var}"
     end
 
     def self.find(id)
       var = self.account_info
       var.each do |num|
-        if num.id == id
+        if num.id == id.to_s
           return num
         end
       end
     end
 
-
-
     def self.account_info
-      accounts = CSV.read('accounts.csv')
-      account_info = []
-
+      account_hash = {:id => nil, :money => nil, :start_date => nil}
+      accounts = CSV.read('accounts.csv') #access into the accounts csv
+      account_info = []  #create blank array that'll store all instances
+      # create a loop to create new instances for each row in the Excel file
       CSV.foreach("accounts.csv") do |row|
         info = self.new(id: row[0].to_f, money: row[1].to_f, start_date: row[2])
         account_info << info
       end
-      return account_info
+      return account_info #pushes each info into the array
     end
-
   end
 
   class Owner
-    # attr_accessor :name, :address,
+    attr_reader :list_num
     def initialize(info_hash)
       @list_num = info_hash[:list_num]
       @last_name = info_hash[:last_name]
@@ -75,38 +79,98 @@ module Bank
       @state = info_hash[:state]
     end
 
+    def self.find(list_num)
+      var = self.owner_info
+      var.each do |hash|
+        if hash.list_num == list_num.to_s
+          return hash
+        end
+      end
+    end
+
     def self.owner_info
+      info_hash = {:list_num => "", :last_name => "",:first_name => "",
+        :street =>"", :city => "", :state => ""}
       owners = CSV.read('owners.csv')
       owner_info = []
+
+      # loop down each row and creates a new hash each time
       CSV.foreach("owners.csv") do |row|
-        info = self.new(list_num: row[0], last_name: row[1], first_name: row[2],
-          street: row[3], city: row[4], state: row[5])
-          owner_info << info
+        hashes = info_hash
+        # loop across each column
+        (0..(owners[0].length)-1).each do |col|
+          hashes[info_hash.keys[col]] = row[col]
+        end
+        # uses self to create instance within Owner
+        info = self.new(hashes)
+        owner_info << info
       end
       return owner_info
     end
 
   end
-end
 
-# refer to csv files for info
-# owner_hash ={
-#   :list_num =>
-#   :name => {
-#     :last_name =>
-#     :first_name =>
-#
-#   }
-#   :address =>{
-#     :street =>
-#     :city =>
-#     :state =>
-#   }
-#   :id =>
-#   :initial_balance =>
-#
-#   :open_info => {
-#     :date =>
-#     :time =>
-#   }
-# }
+  class SavingsAccount < Account
+    def initialize(hash)
+      @id = hash[:id]
+      @money = hash[:money]
+      @start_date = hash[:start_date]
+
+      if money < 10
+        raise ArgumentError.new("Only amounts above $10 are allowed in the bank.")
+      end
+    end
+
+    def withdraw(take_out = 0)
+      # @take_out = take_out
+      if @money.to_f - take_out.to_f < 10#@take_out.to_f < 0
+        puts "$10 must be left in the savings account at all times."
+        balance
+      else
+        @money -= take_out.to_f + 2
+        balance
+      end
+    end
+
+    def add_interest(rate = 0.25)
+      interest = (@money * rate / 100) - @money
+      print "Total interest: $#{'%0.7s' % @interest.to_s}"
+    end
+  end
+
+  class CheckingAccount < Account
+    def initialize(hash)
+      super
+      @num = 0
+    end
+
+    def withdraw(take_out = 0)
+      # @take_out = take_out
+      if @money.to_f - take_out.to_f < 0#@take_out.to_f < 0
+        puts "You don't have enough money in the bank for this transaction."
+        balance
+      else
+        @money -= take_out.to_f + 1
+        balance
+      end
+    end
+
+    def withdraw_using_check(take_out = 0)
+      @num += 1
+      if @money.to_f - take_out.to_f < -10#@take_out.to_f < 0
+        puts "You may only overdraft up to $10."
+        balance
+      elsif @num > 3
+        @money -= take_out.to_f + 2
+        balance
+      else
+        @money -= take_out.to_f
+        balance
+      end
+    end
+
+    def reset_checks
+      @num = 0
+    end
+  end
+end
