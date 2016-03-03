b56e763a0d76cd517b9a07000112a756a1dbdb86
Alysia Brown <alysiacbrown@gmail.com>
2016-03-03 14:06:53 -0800

    Updated SavingsAccount withdraw method.

--- bank_accounts.rb
+++ bank_accounts.rb
@@ -1,12 +1,7 @@
 #Wave 3 Requirements
 
-#1. Create a SavingsAccount class which should inherit behavior from the Account class.
 #-It should include the following updated functionality:
-# => The initial balance cannot be less than $10. If it is, this will raise an #ArgumentError
 
-# => Updated withdrawal functionality:
-# =>    - Each withdrawal 'transaction' incurs a fee of $2 that is taken out of the         balance.
-# =>    - Does not allow the account to go below the $10 minimum balance - Will output a warning #message and return the original un-modified balance
 #-It should include the following new methods:
 # => add_interest(rate): Calculate the interest on the balance and add the interest to the balance. Return the interest that was calculated and added to the balance (not the updated balance).
 # =>    - Input rate is assumed to be a percentage (i.e. 0.25).
@@ -32,14 +27,19 @@ module Bank
       @user_id = user_id
       @open_date = open_date
       @balance = initial_balance    # Renamed initial balance variable to balance
-      if initial_balance <= 0       # for clarity as the balance will change over time.
-        error_initial_balance       # Cannot have a negative initial balance.
+      initial_balance_check
+    end
+
+    def initial_balance_check
+      if @balance <= 0       # for clarity as the balance will change over time.
+        raise ArgumentError, "Initial balance argument must be greater than $0."
       end
     end
 
     def withdraw(amount_to_withdraw)
       if amount_to_withdraw >= @balance #cannot put the account into negative balance
-        error_withdraw
+        puts "You may not withdraw that amount as the account must maintain a positive balance. Your account balance is $#{@balance} Please select a different amount."
+        @balance
       else
         @balance = @balance - amount_to_withdraw
       end
@@ -92,15 +92,29 @@ module Bank
         end
         end
     end
+  end # Account class end.
 
+  class SavingsAccount < Account
 
-    def error_initial_balance
-      raise ArgumentError, "Initial balance argument must be greater than 0."
+    def initial_balance_check
+      if @balance <= 10
+        raise ArgumentError, "Initial balance argument must be greater than $10."
+      end
     end
 
-    def error_withdraw
-      puts "You may not withdraw that amount as you only have $#{@balance} in your account."
-      @balance
+    # Each withdrawal 'transaction' incurs a fee of $2 that is taken out of the balance.
+    def withdraw(amount_to_withdraw)
+#      WITHDRAW_FEE == 2    This returned an error stating uninitialized constant??
+      if @balance - amount_to_withdraw < 10 #cannot go below $10 balance
+        puts "You may not withdraw that amount as the account must maintain a balance above $10. Your current balance is $#{@balance}. Please select a different amount to withdraw."
+        @balance  # Not sure super - WITHDRAW_FEE will work since it would still
+      else        # deduct the withdraw fee even if they get negative balance error
+        @balance = @balance - amount_to_withdraw - 2  #$2 withdrawal fee. Tried using
+      end                                             #a constant but got an error-_-
     end
-  end
-end
+
+
+  end #SavingsAccount class end.
+
+
+end # Module end.
