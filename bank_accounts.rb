94236a571ab063d870717fd709c5b0931493d5b5
Suzanne Harrison <harrison_family@hotmail.com>
2016-03-02 16:29:16 -0800

    entered Owner self.all and self.find--works

--- bank_accounts.rb
+++ bank_accounts.rb
@@ -20,7 +20,7 @@
       end
     end
 
-    def self.all(filename)
+    def self.all(filename = "./support/accounts.csv")
       all_accounts = []
       CSV.open(filename, 'r') do |csv|
         csv.read.each do |line|
@@ -70,15 +70,38 @@
   end
 
   class Owner
-    attr_accessor :id, :name, :street_address, :street_address_2, :city, :zip_code, :phone
+    attr_accessor :id, :first_name, :last_name, :street_address, :city, :state
 
     def initialize(owner)
+      @owner_number = owner[:owner_number]
       @first_name = owner[:first_name]
-      @slast_name = owner[:last_name]
+      @last_name = owner[:last_name]
       @street_address = owner[:street_address]
       @city = owner[:city]
       @state = owner[:state]
     end
+
+    def self.all(filename = "./support/owners.csv")
+      all_owners = []
+      CSV.open(filename, 'r') do |csv|
+        csv.read.each do |line|
+        all_owners << self.new(owner_number: line[0].to_s, last_name: line[1], first_name: line[2], street_address: line[3], city: line[4], state: line[5])
+        end
+      end
+      return all_owners
+    end
+
+    def self.find(owner_number, filename = "./support/owners.csv")
+      CSV.foreach(filename, 'r') do |line|
+        #csv.read.each do |line|
+            if line[0].to_s == owner_number.to_s
+              selected_owner = self.new(owner_number: line[0].to_s, last_name: line[1], first_name: line[2], street_address: line[3], city: line[4], state: line[5])
+              return selected_owner
+            end
+            #self.new(id: line[0], initial_balance: line[1].to_f, start_date: line[2])
+      end
+    end
+
   end
 end
 
@@ -87,7 +110,7 @@ end
 #my_account.pass_owner_info(owner) #to pass owner info into Account
 
 #to test the self and csv method
-test = Bank::Account.all("./support/accounts.csv")
+test = Bank::Owner.all
 ap test
 
 #Bank::Account.find(1212, "./support/accounts.csv")
