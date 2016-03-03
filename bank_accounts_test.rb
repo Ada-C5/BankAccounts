91606deafaf8689f999f085e49c984628874d835
Leah <leahnpetersen@gmail.com>
2016-03-02 16:14:37 -0800

    Added more tests

--- bank_accounts_test.rb
+++ bank_accounts_test.rb
@@ -1,10 +1,28 @@
+require_relative 'bank_account.rb'
+# # # # TESTS FOR ACCOUNT CLASS # # # # #
+### TEST FOR SELF.ALL METHOD   ###
+puts Bank::Account.all("./support/accounts.csv")
+
+
+
 # # # # TESTS FOR OWNER CLASS # # # # #
+### TEST FOR SELF.CREATE_OWNERS(file)  ###
+puts Bank::Owner.create_owners("./support/owners.csv")
+
+
+### TEST FOR SELF.ALL METHOD   ###
+puts Bank::Owner.all("./support/owners.csv")
+
 ###   TEST FOR FIND METHOD   ###
 my_owner = Bank::Owner.find(14)
-puts my_owner
+puts "Using find method the owner instance with id 14 is #{my_owner}"
 
 
 ###   TEST FOR GET_ACCOUNT METHOD   ###
 my_owner = Bank::Owner.find(14)
 account = my_owner.get_account
-puts account
+puts "Using get_account method this account instance for id 14 is #{account}"
+
+### TEST FOR GET_INFO METHOD ###
+my_owner = Bank::Owner.find(14)
+puts my_owner.get_info
