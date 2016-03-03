0ae0471a330e0eeabeb1bb28b126708400af04f5
Leah <leahnpetersen@gmail.com>
2016-03-02 16:34:43 -0800

    Added test to get balance of account using owner id

--- bank_accounts_test.rb
+++ bank_accounts_test.rb
@@ -1,27 +1,28 @@
 require_relative 'bank_account.rb'
 # # # # TESTS FOR ACCOUNT CLASS # # # # #
 ## TEST FOR SELF.ALL METHOD   ###
-puts Bank::Account.all("./support/accounts.csv")
+# puts Bank::Account.all("./support/accounts.csv")
 
 
 
-# # # # TESTS FOR OWNER CLASS # # # # #
-### TEST FOR SELF.CREATE_OWNERS(file)  ###
-puts Bank::Owner.create_owners("./support/owners.csv")
+# # # # # TESTS FOR OWNER CLASS # # # # #
+# ### TEST FOR SELF.CREATE_OWNERS(file)  ###
+# puts Bank::Owner.create_owners("./support/owners.csv")
 
 
-### TEST FOR SELF.ALL METHOD   ###
-puts Bank::Owner.all("./support/owners.csv")
+# ### TEST FOR SELF.ALL METHOD   ###
+# puts Bank::Owner.all("./support/owners.csv")
 
-###   TEST FOR FIND METHOD   ###
-my_owner = Bank::Owner.find(14)
-puts "Using find method the owner instance with id 14 is #{my_owner}"
+# ###   TEST FOR FIND METHOD   ###
+# my_owner = Bank::Owner.find(14)
+# puts "Using find method the owner instance with id 14 is #{my_owner}"
 
 
 ###   TEST FOR GET_ACCOUNT METHOD   ###
+# get balance of first account with owner id
 my_owner = Bank::Owner.find(14)
-puts my_owner.get_account
+puts my_owner.get_account[0].balance
 
-### TEST FOR GET_INFO METHOD ###
-my_owner = Bank::Owner.find(14)
-puts my_owner.get_info
+# ### TEST FOR GET_INFO METHOD ###
+# my_owner = Bank::Owner.find(14)
+# puts my_owner.get_info
