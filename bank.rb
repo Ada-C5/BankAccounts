10643282d12386c1c2097d904b7e882efe476d67
Melissa Jimison <melissajimison@gmail.com>
2016-03-02 16:27:01 -0800

    After trying so me loops, I am almost sure this loop is the one the actual creates all the Accounts

--- bank.rb
+++ bank.rb
@@ -1,32 +1,33 @@
+require 'csv'
 module Bank
   class Account
-    attr_reader :id, :updated_balance
-    attr_accessor :owner
-    def initialize(id, initial_balance)
-      @id = id
-      @updated_balance = initial_balance
-      if !initial_balance.is_a?(Numeric)
-       raise ArgumentError.new("A new account cannot be created with initial negative balance")
+
+
+    def self.all
+      array_accounts = []
+      CSV.read('support/accounts.csv').each do |i|
+        i = self.new
+        array_accounts << i
       end
-      @owner = owner
+        # puts array_accounts
     end
 
     def withdraw(withdraw)
-      @updated_balance = @updated_balance - withdraw
-      if @updated_balance < 0
-        @updated_balance = @updated_balance + withdraw
+      @balance = @balance - withdraw
+      if @balance < 0
+        @balance = @balance + withdraw
         puts "You dont have all that money"
       end
       balance
     end
 
     def deposit(deposit)
-      @updated_balance = @updated_balance + deposit
+      @balance = @balance + deposit
       balance
     end
 
     def balance
-      puts "#{@updated_balance} is your new balance"
+      puts "#{@balance} is your new balance"
     end
 
 
@@ -46,7 +47,7 @@ module Bank
 
 end
 
-melissa_account = Bank::Account.new(123, 5400)
-
-melissa = Bank::Owner.new(name: "Melissa", last_name: "Jimison", email: "mjimison@gmail.com")
-melissa = Bank::Owner.new(name: "David", last_name: "Quintero", email: "djimison@gmail.com")
+melissa_account = Bank::Account.new
+melissa_account.deposit(100)
+# melissa = Bank::Owner.new(name: "Melissa", last_name: "Jimison", email: "mjimison@gmail.com")
+# melissa = Bank::Owner.new(name: "David", last_name: "Quintero", email: "djimison@gmail.com")
