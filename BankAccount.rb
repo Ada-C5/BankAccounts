7f759202e0ac948880c0aa2fc7b96eca0ad91613
Mindy Carson <mcarson1111@gmail.com>
2016-03-02 18:37:38 -0800

    Bank account is working, commented out wave 2 #1 hahahaha

--- BankAccount.rb
+++ BankAccount.rb
@@ -1,16 +1,16 @@
-require 'csv'
+#require 'csv'
 
-#module Bank
+module Bank
   class Account
-    attr_accessor :id, :initial_balance, :accounts, :creation_date #these are like the instance variables
+    attr_accessor :id, :balance, :creation_date #these are like the instance variables
 
-    def initialize (account_info)
-      @id = account_info[:id]
-      @balance = account_info[:initial_balance]
+    def initialize (account_info)   #initialize is actually a class method 
+      @id = account_info[:id].to_i
+      @balance = account_info[:initial_balance].to_i
         if @balance < 0
           raise ArgumentError.new("Nope.") #now, how do i not let initial_balance be a negative? Start over?
         end
-      @creation_date = account_info[:date] #these are the hash key
+      @creation_date = account_info[:creation_date] #these are the hash key
 
     end
 
@@ -20,17 +20,17 @@ require 'csv'
      #and on each iteration put the cooresponding indexes together in a new array
      #since each line in the csv file is a seperate array, i want to return the ______
 
-    def create_new(csv_index)
-       account_array = []
-       account_array = CSV.read("./support/accounts.csv", 'r')
-        #So this is going through the file and it's saying that whatever the person choses for the csv_index,
-        #look at that index and go through that index and assign each of the internal indexes to instance variables****
-        @id = account_array[csv_index][0]
-        @balance = account_array[csv_index][1]
-        @open_date = account_array[csv_index][2]
-
-       puts "#{@id}, #{@balance}, #{@open_date}."
-    end
+    # def create_new(csv_index)
+    #    account_array = []
+    #    account_array = CSV.read("./support/accounts.csv", 'r')
+    #     #So this is going through the file and it's saying that whatever the person choses for the csv_index,
+    #     #look at that index and go through that index and assign each of the internal indexes to instance variables****
+    #     @id = account_array[csv_index][0]
+    #     @balance = account_array[csv_index][1]
+    #     @open_date = account_array[csv_index][2]
+    #
+    #    puts "#{@id}, #{@balance}, #{@open_date}."
+    # end
 
 
 
@@ -94,8 +94,8 @@ require 'csv'
   end
 
 
-  my_account = Account.new
-  my_account.create_new(4)
+  # my_account = Account.new
+  # my_account.create_new(4)
 
 
 
@@ -105,7 +105,7 @@ require 'csv'
 
 
 
-#end
+end
 
 
   # class Owner
@@ -119,6 +119,11 @@ require 'csv'
   #   end
   #
   # end
+
+#1212,1235667,1999-03-27 11:30:09 -0800
+
+account_hash = {id:"1212", initial_balance:"1235667", creation_date:"1999-03-27"}
+
 
 
 #Bank::Account.new(1111, 4000)
