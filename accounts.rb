a64999c5f647a008a2c03f01980ac4a9000a6c87
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-03-04 14:26:17 -0800

    Refactor 1 complete - behavior over data, private methods hide balance and crazy conditionals

--- accounts.rb
+++ accounts.rb
@@ -3,8 +3,8 @@ require 'csv'
 #require_relative './support/accounts'
 module Bank
  class Account
-   attr_accessor :balance, :accounts_hash
-   attr_reader :id, :owner
+   attr_accessor :accounts_hash
+   attr_reader :id, :owner, :balance
 
    def initialize(id_number, initial_balance, account_date)
      unless initial_balance.to_f > 0
@@ -19,6 +19,9 @@ module Bank
   CSV.read('./support/accounts.csv')
   end
 
+  def self.create_new
+  end
+
   def self.all
     accounts_hash = {}
     self.pull_from_csv.each do |account|
@@ -30,16 +33,16 @@ module Bank
   end
 
    def withdraw(amount)
-     if amount > @balance
+     if amount > balance
        puts "Warning: requested amount is greater than available balance. Please make another selection."
-       return @balance
+       return balance
      else
-       @balance = @balance - amount
+       balance(amount, "withdraw")
      end
    end
 
    def deposit(amount)
-     @balance = @balance + amount
+    balance(amount, "deposit")
    end
 
    def self.find(id)
@@ -49,6 +52,21 @@ module Bank
        end
      end
      puts "Account not found."
+   end
+
+   def check_balance(id) #best way??
+     balance
+   end
+
+   private
+   def balance(amount = 0, transaction_type = nil, fee = 0)
+     if transaction_type == "deposit"
+       @balance = @balance + amount
+     elsif transaction_type == "withdraw"
+       @balance = @balance - amount - fee
+     else
+       return @balance
+     end
    end
 
  end
@@ -100,9 +118,8 @@ module Bank
    def self.find_owner_number(id) #search accounts by id number
      owner_list = CSV.read('./support/account_owners.csv')
      owner_list.each do |account|
-       puts account
       if account[0] == id.to_s
-        puts "This is the number associated with #{id}'s account"
+        puts "This is the number associated with #{id}'s account:"
         return account[1] #returns owner number
       end
     end
@@ -123,16 +140,23 @@ module Bank
    end
 
    def withdraw(amount)
-     puts "Available balance: #{@balance - 10}" #accounts for 10 minimum rule
-     if (amount + 2) > (@balance - 10) #account for ability to withdraw $2 fee
+     puts "Available balance:" #accounts for 10 minimum rule
+     puts balance
+     if (amount + 2) > (balance - 10) #account for ability to withdraw $2 fee
        puts "Warning: requested amount is greater than available balance. Please make another selection, and be aware that withdrawl from Savings results in a $2 fee, which your balance must cover in addition to your withdrawl amount"
-       return @balance
+       balance
      else
-       puts "Successfully withdrew #{amount} plus $2 fee."
-       @balance = @balance - (amount + 2)
+       balance(amount, "withdraw", 2)
+       puts "Successfully withdrew $#{amount} plus $2 fee."
+      #  @balance = @balance - (amount + 2)
      end
    end
 
+   def interest(rate)
+     add_interest(rate) #Not sure if this is a good idea or a dumb idea
+   end
+
+   private
    def add_interest(rate)
      @balance =  balance * (rate/100)
      return (@balance * (rate/100))
@@ -150,35 +174,49 @@ module Bank
 
 
    def withdraw(amount)
-     puts "Available balance: #{@balance}"
-     if (amount + 1) > (@balance) #account for ability to withdraw $2 fee
+     puts "Available balance: "
+     puts balance
+     if (amount + 1) > balance #account for ability to withdraw $2 fee
        puts "Warning: requested amount is greater than available balance. Please make another selection, and be aware that withdrawal from Checking results in a $1 fee, which your balance must cover in addition to your withdrawal amount"
-       return @balance
+       balance
      else
+       balance(amount, "withdraw", 1)
        puts "Successfully withdrew #{amount} from Checking plus $1 fee."
-       @balance = @balance - (amount + 1)
+      #  @balance = @balance - (amount + 1)
      end
    end
 
    def withdraw_using_check(amount)
-     puts "Available balance: #{@balance}" #accounts for 10 minimum rule
-     if (amount > (@balance + 10) || ( (@number_checks > 2) && ((amount + 2) > (@balance + 10))))#account for ability to overdraft to -10
+     puts "Available balance:"
+     puts balance
+     #accounts for 10 minimum rule:
+     if amount_too_large(amount)
        puts "Warning: requested amount is greater than available balance. Please make another selection, and be aware that withdrawal from Checking results in a $1 fee, which your balance must cover in addition to your withdrawal amount"
-       return @balance
-     elsif @number_checks > 2 && ((amount + 2) < (@balance + 10))
+       balance
+     elsif free_checks_used_up(amount)
+       balance(amount, "withdraw", 2) #$2 fee for going over allotted number checks
        puts "Successfully withdrew #{amount} from Checking plus $2 fee for checks after the 3 monthly allowance."
-       @balance = @balance - amount - 2 #$2 fee for check
      else
+       balance(amount, "withdraw") #no fee
        puts "Successfully withdrew #{amount} from Checking. #{3-@number_checks} free check(s) remaining this month"
        @number_checks += 1
-       @balance = @balance - amount
+       balance
      end
    end
 
    def reset_checks
      @number_checks = 0
    end
+
+   private
+   def amount_too_large(amount)
+     (amount > (balance + 10) || ( (@number_checks > 2) && ((amount + 2) > (balance + 10))))#account for ability to overdraft to -10
+   end
+
+   def free_checks_used_up(amount)
+     @number_checks > 2 && ((amount + 2) < (balance + 10))
+   end
+
  end
 
 end
-#Does not allow the account to go below the $10 minimum balance - Will output a warning message and return the original un-modified balance
