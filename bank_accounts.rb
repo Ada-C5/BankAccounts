0dd8c9c149ddc2498287d58b3a3b393d23c4de24
Valerie <val.conklin@gmail.com>
2016-03-04 23:37:09 -0800

    Added check_withdraw_type and  check_against_minimum methods to Account class, then linked child classes to that. Cleaned up code and deleted all superfluous comments and methods.

--- bank_accounts.rb
+++ bank_accounts.rb
@@ -1,6 +1,4 @@
 require 'csv'
-require 'money'
-I18n.enforce_available_locales = false
 module Bank
 
   class Account
@@ -8,32 +6,22 @@ module Bank
     MINIMUM_STARTING_BALANCE = 0
 
     def initialize(account_info)
-      # manually choose the data from the first line of the CSV file and ensure
-      # you can create a new instance of your Account using that data
-
-      # create a hash with keys id, balance, date_created and get those values
-      # from reading accounts.csv (which turns it into an array), and use indexes
-      # of given array to shovel into account_info hash?
-
       if account_info[:balance] < MINIMUM_STARTING_BALANCE
        raise ArgumentError.new("You must have at least $#{MINIMUM_STARTING_BALANCE} to open an account.")
       end
       @id = account_info[:id]
       @balance = account_info[:balance]
       @date_created = account_info[:date_created]
-      # ADD THIS BACK IN LATER
-      # @owner = account_info[:owner] # temporary value until Owner is created
     end
 
     def self.all
-      # this is what you'd do if working from a CSV file... not related to the instances we've initialized for testing
+      # this is what you'd do if working from a CSV file... not related to the instances we've initialized for testing.
       # pulls in the data from the CSV file as an array of arrays
       array_of_accounts = CSV.read("support/accounts.csv")
       account_info_array = []
-      # this method should return an array of hashes, where each hash represents one row of data
-      # array_of_accounts.each do.... make a hash {id => element[0], balance => element[1], }
-      # instantiate a new Account based on that hash, after each one shovel into an array
-      # return that array to whoever calls this method
+      # this method should return an array of hashes, where each hash represents one row of data.
+      # instantiates a new Account based on each hash, after each one it shovels into an array and
+      # returns that array to whoever calls this method
       array_of_accounts.each do |element|
         account_info_array << Account.new({id: element[0], balance: element[1].to_i, date_created: element[2]})
       end
@@ -41,10 +29,10 @@ module Bank
     end
 
     def self.find(given_id)
-      #returns an instance of Account where the value of the id field in the CSV
-      #matches the passed parameter
+      # returns an instance of Account where the value of the id field in the CSV
+      # matches the passed parameter
 
-      # iterate over the Account instances until you find the instance with the matching id
+      # iterates over the Account instances until you find the instance with the matching id
       # all the account instances are listed in account_info_array
 
       # the problem with this is it generates the accounts from the CSV every time.
@@ -59,54 +47,43 @@ module Bank
       end
     end
 
-    def convert_cents(money)
-      Money.new(money, "USD")
-    end
-
     # If the owner has already been created in the Owner class, the method should be called like so:
-    # account_instance.add_owner(owner_instance.name)
+    # @account_instance.add_owner(owner_instance.name)
     def add_owner(owner_name)
       @owner = owner_name
       #Bank::Owner.name
     end
 
     def withdraw(amount_to_withdraw, minimum_balance=0, transaction_fee=0)
-      if @balance - amount_to_withdraw < minimum_balance
-        raise ArgumentError.new("This withdrawal would go below the minimum balance.")
-      end
+      check_withdraw_type("account", amount_to_withdraw, minimum_balance, transaction_fee)
       @balance = @balance - amount_to_withdraw
-      # show_balance
     end
 
-    ### BRB REFACTORING
-    # def withdraw(amount_to_withdraw, instance_var)
-    #   if amount_to_withdraw > @balance
-    #       raise ArgumentError.new("This withdrawal would cause a negative balance.
-    #       Do not attempt.")
-    #   end
-    #   @balance = @balance - amount_to_withdraw
-    #   # included the following because otherwise "super" in the SavingsAccount
-    #   # and CheckingAccount withdrawal methods will do show_balance twice, once
-    #   # before the withdrawal has even occurred... and that's annoying :|
-    #   unless instance_var.is_a?(SavingsAccount) || instance_var.is_a?(CheckingAccount)
-    #     show_balance
-    #   end
-    #   return @balance
-    # end
+# uses the type of withdraw ("from savings", "using a check", etc.) to determine if the user is overdrafting
+    def check_withdraw_type(withdraw_type, amount_to_withdraw, minimum_balance, transaction_fee)
+      case withdraw_type
+      when "account", "checking", "savings"
+        check_against_minimum(amount_to_withdraw, minimum_balance, transaction_fee)
+      when "check"
+        check_against_minimum(amount_to_withdraw, minimum_balance, transaction_fee)
+        if @check_count >= 3
+          transaction_fee = 200
+          check_against_minimum(amount_to_withdraw, minimum_balance, transaction_fee)
+          @balance = @balance - transaction_fee
+        end
+      end
+    end
+
+# compares balance with minimum balance and doesn't let the user continue if they try
+# to go over
+    def check_against_minimum(amount_to_withdraw, minimum_balance, transaction_fee)
+      if (@balance - amount_to_withdraw - transaction_fee) < minimum_balance
+        raise ArgumentError.new("This withdrawal would go below the minimum balance.")
+      end
+    end
 
     def deposit(amount_to_deposit)
       @balance = @balance + amount_to_deposit
-      show_balance
-      return @balance
-    end
-
-    # displays the balance in a nice user-friendly way, but also returns it to the other methods
-    def show_balance
-      # temporarily converts @balance to dollar format for easier readability,
-      # but doesn't affect original numbers. This way all math can still be done
-      # in cents.
-      pretty_money = convert_cents(@balance)
-      puts "The balance for this account is currently $#{pretty_money}."
     end
 
   end
@@ -119,41 +96,20 @@ module Bank
     end
 
     def withdraw(amount_to_withdraw, minimum_balance=0, transaction_fee=100)
-      if super - transaction_fee
-        raise ArgumentError.new("This withdrawal would go below the minimum balance.")
-      end
+      check_withdraw_type("checking", amount_to_withdraw, minimum_balance, transaction_fee)
       @balance = super - transaction_fee
     end
 
     def withdraw_using_check(amount_to_withdraw, minimum_balance=-1000, transaction_fee=0)
-      # does this first so the final value returned is @balance
-      @check_count += 1
+      check_withdraw_type("check", amount_to_withdraw, minimum_balance, transaction_fee)
       @balance = withdraw(amount_to_withdraw, minimum_balance, transaction_fee)
-      if @check_count > 3
-        transaction_fee = 200
-        @balance = @balance - transaction_fee
-      end
-      # why do I need return @balance here? why does it return nil otherwise?
+      @check_count += 1
       return @balance
     end
 
-    ### BRB REFACTORING
-    # def withdraw(amount_to_withdraw, instance_var)
-    #     @balance = super - TRANSACTION_FEE
-    #     show_balance
-    #     return @balance
-    # end
-
-
-
-    ### BRB REFACTORING
-    # def withdraw_using_check(amount_to_withdraw, instance_var)
-    #   minimum_balance = -1000
-    #   if (@balance - amount_to_withdraw) < minimum_balance
-    #     raise ArgumentError.new("Withdrawal by check only allows accounts to go
-    #     into overdraft up to -$10. Do not attempt.")
-    #   end
-
+    def reset_checks
+      @check_count = 0
+    end
 
   end
 
@@ -161,25 +117,17 @@ module Bank
     MINIMUM_STARTING_BALANCE = 1000
 
     # The initial balance cannot be less than $10.
-    def initialize(account_info)
-      if account_info[:balance] < MINIMUM_STARTING_BALANCE
-        raise ArgumentError.new("You must have at least $10 to open a savings account.")
-      end
+     def initialize(account_info)
+       if account_info[:balance] < MINIMUM_STARTING_BALANCE
+         raise ArgumentError.new("You must have at least $#{MINIMUM_STARTING_BALANCE} to open a savings account.")
+       end
       super
-    end
+     end
 
-    ### BRB REFACTORING
-    # def withdraw(amount_to_withdraw, instance_var)
-    #   # Does not allow the account to go below the $10 minimum balance - Will output a warning message
-    #   if (@balance - amount_to_withdraw) < MINIMUM_BALANCE
-    #     raise ArgumentError.new("Savings accounts must maintain at least
-    #     $#{convert_cents(MINIMUM_BALANCE)}. Do not attempt.")
-    #   else
-    #     @balance = super - TRANSACTION_FEE
-    #     show_balance
-    #     return @balance
-    #   end
-    # end
+     def withdraw(amount_to_withdraw, minimum_balance=1000, transaction_fee=200)
+       check_withdraw_type("savings", amount_to_withdraw, minimum_balance, transaction_fee)
+       @balance = super - transaction_fee
+     end
 
     def add_interest(rate)
       interest = calculate_interest(rate)
@@ -204,8 +152,9 @@ module Bank
   end
 end
 
-# for testing the different methods in IRB?
+# for testing the different methods in IRB, play with them!
+# when using in IRB, these instance names must have @ sign before them, but not when run in ruby
 @checking_instance = Bank::CheckingAccount.new({id: "1001", balance: 20045, date_created: "March 5, 2016"})
-@savings_instance = Bank::SavingsAccount.new({id: "1000", balance: 55000, date_created: "March 5, 2016"})
+@savings_instance = Bank::SavingsAccount.new({id: "1000", balance: 130000, date_created: "March 5, 2016"})
 @account_instance = Bank::Account.new({id: "0999", balance: 20000, date_created: "March 3, 2016"})
 @owner_instance = Bank::Owner.new("Barbara Thompson", "545-665-5535", "looploop@loo.org", "5545 Apple Drive Issaquah, WA 98645")
